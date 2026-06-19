492f7372fb827b6c4d049a03ecfff9f8c62f0d30
Sanjay <sankir25092007gmail.com>
2026-06-19 23:29:13 +0530

    Add actual source code

---
+++ myapp/create_db.py
@@ -0,0 +1,12 @@
+import asyncio
+from database import engine
+from models import Base
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(create_db())
+
+
+async def main():
